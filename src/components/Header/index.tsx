7c9db10b1dfabaf817f8776d0f7c3d73ab3349ee
Henriko Alberton <henriko.foz@gmail.com>
2025-03-15 06:47:26 -0300

    chore: correct homepage and link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { LanguageSelector } from './LanguageSelector'
 import { ModeToggle } from '@src/components/Header/ThemeModeSelector'
+import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
     <div className="fixed left-0 top-0 flex w-full items-center justify-between border bg-slate-50 bg-opacity-70 px-4 py-4 md:px-12">
-      <a href="/" className="text-xs md:text-base">
+      <Link to="/" className="text-xs md:text-base">
         Vite React TS Tailwind Starter
-      </a>
+      </Link>
       <div className="flex items-center gap-4">
         <LanguageSelector />
         <ModeToggle />
