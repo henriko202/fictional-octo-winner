63d79b11ddb6808d36a3cc0eb31eb79b6742e221
Henriko Alberton <henriko.foz@gmail.com>
2025-03-15 06:08:29 -0300

    chore: update resources

--- src/types/resources.ts
+++ src/types/resources.ts
@@ -1,8 +1,10 @@
-import translation from '@i18n/locales/en/translation.json'
 import notFound from '@i18n/locales/en/notFound.json'
+import task from '@i18n/locales/en/task.json'
+import translation from '@i18n/locales/en/translation.json'
 
 const resources = {
   notFound,
+  task,
   translation,
 } as const
 
