bd7fb06ea4907a897e738a87126ba191995c794a
Henriko Alberton <henriko.foz@gmail.com>
2025-03-15 06:17:58 -0300

    Updates

---
+++ assets/notFound-bd39469f.js
@@ -0,0 +1,1 @@
+const o="Oops!",e="Sorry, an unexpected error has occurred.",t="Back to home page",c={oops:o,title:e,backToHome:t};export{t as backToHome,c as default,o as oops,e as title};
---
+++ assets/translation-2332cc04.js
@@ -0,0 +1,1 @@
+const t="fictional-octo-winner",n={title:t};export{n as default,t as title};
