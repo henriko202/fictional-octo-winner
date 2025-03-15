046eea3b606af53073c85dd691796d56e5f0f2f0
Henriko Alberton <henriko.foz@gmail.com>
2025-03-15 03:42:32 -0300

    chore: change delete color

--- src/components/TaskList/components/DeleteTodo.tsx
+++ src/components/TaskList/components/DeleteTodo.tsx
@@ -1,5 +1,6 @@
 import { Trash2 } from 'lucide-react'
 import { TodosMutations } from '@src/components/TaskList/mutations'
+import { cn } from '@src/utils'
 
 type DeleteTodoProps = {
   id: number
@@ -10,11 +11,11 @@ export const DeleteTodo = ({ id }: DeleteTodoProps) => {
 
   return (
     <span className="cursor-pointer" onClick={async () => await deleteTodoMutation.deleteTodo({ id })}>
-      {deleteTodoMutation.isLoading && deleteTodoMutation.variables?.id === id ? (
-        <Trash2 className="h-5 w-5 animate-spin text-primary" />
-      ) : (
-        <Trash2 className="h-5 w-5 text-primary" />
-      )}
+      <Trash2
+        className={cn('h-5 w-5 text-red-700', {
+          'animate-spin': deleteTodoMutation.isLoading && deleteTodoMutation.variables?.id === id,
+        })}
+      />
     </span>
   )
 }
